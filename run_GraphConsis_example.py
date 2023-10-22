19a9adde7fdb0c3371caf32831b9bc67015b0820
B.Mohith Siva Sai <92243592+B-mohith@users.noreply.github.com>
2023-10-22 15:17:27 +0530

    Update run_GraphConsis_example.py

--- run_GraphConsis_example.py
+++ run_GraphConsis_example.py
@@ -124,7 +124,7 @@ def main():
                                               torch.FloatTensor(valid_r))
     testset = torch.utils.data.TensorDataset(torch.LongTensor(test_u), torch.LongTensor(test_v),
                                              torch.FloatTensor(test_r))
-    train_cluster_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, partition_assignments=partition_assignments)
+    train_cluster_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, partition_assignments=partition_graph(adjacency_matrix, 3))
     valid_loader = torch.utils.data.DataLoader(validset, batch_size=args.test_batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=True)
     num_users = history_u_lists.__len__()
